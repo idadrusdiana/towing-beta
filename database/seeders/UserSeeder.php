e49ebef619db42692e1752bac0ca5348e8628835
Idad Rusdiana <idadrusdiana01@gmail.com>
2024-09-09 16:11:24 +0700

    BE/userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,9 +8,6 @@ use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run()
     {
         $admin = User::create([
